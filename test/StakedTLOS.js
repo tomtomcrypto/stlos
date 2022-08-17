0fcf7c80bafec94aac0cd9b56a0c4da838862b0c
Jesse Schulman <jesse@caleos.io>
2022-08-17 10:43:33 -0700

    Adding more tests for rounding

--- test/StakedTLOS.js
+++ test/StakedTLOS.js
@@ -32,13 +32,37 @@ describe("StakedTLOS", function () {
     });
 
 
-    it("Should observe yield only increases", async function () {
+    it("Should observe yield only increases with small numbers", async function () {
       const [owner, addr1, addr2] = await ethers.getSigners();
 
       await stlos.asset();
       await stlos.depositTLOS({value: ethers.utils.parseEther("1.0")});
-      console.log("Checking balanceOf owner to == 1");
-      expect(await stlos.balanceOf(owner.address)).to.equal(ethers.utils.parseEther("1.0"), "Owner address shoudl get 1 sTLOS for depositing 1 TLOS");
+      expect(await stlos.balanceOf(owner.address)).to.equal(ethers.utils.parseEther("1.0"), "Owner address should get 1 sTLOS for depositing 1 TLOS");
+      expect(await stlos.maxWithdraw(owner.address)).to.equal(ethers.utils.parseEther("1.0"), "Owner address should be able to withdraw 1 TLOS after depositing 1 TLOS");
+
+      await owner.sendTransaction({
+        to: stlos.address,
+        value: ethers.utils.parseEther("1.0"),
+      });
+
+      expect(await stlos.maxWithdraw(owner.address)).to.equal(ethers.utils.parseEther("2.0"), "Owner should have max withdraw of 101 after depositing 1 TLOS and 100 TLOS being added as yield");
+
+      await stlos.connect(addr1).depositTLOS({value: ethers.utils.parseEther("1.0")});
+
+      const addr1Balance = await stlos.balanceOf(addr1.address);
+
+      expect(await stlos.previewRedeem(addr1Balance)).to.equal(ethers.utils.parseEther("1.0"), "Address 1 should be able to withdraw 1 TLOS after depositing 1 TLOS");
+      expect(await stlos.maxWithdraw(addr1.address)).to.equal(ethers.utils.parseEther("1.0"), "Address 1 should be able to withdraw 1 TLOS after depositing 1 TLOS");
+
+      expect(await stlos.maxWithdraw(owner.address)).to.equal(ethers.utils.parseEther("2.0"), "Owner should still have max withdrawl of 101 after address 1 deposited 1");
+    });
+
+    it("Should observe yield only increases with larger numbers", async function () {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+
+      await stlos.asset();
+      await stlos.depositTLOS({value: ethers.utils.parseEther("1.0")});
+      expect(await stlos.balanceOf(owner.address)).to.equal(ethers.utils.parseEther("1.0"), "Owner address should get 1 sTLOS for depositing 1 TLOS");
       expect(await stlos.maxWithdraw(owner.address)).to.equal(ethers.utils.parseEther("1.0"), "Owner address should be able to withdraw 1 TLOS after depositing 1 TLOS");
 
       await owner.sendTransaction({
@@ -49,7 +73,10 @@ describe("StakedTLOS", function () {
       expect(await stlos.maxWithdraw(owner.address)).to.equal(ethers.utils.parseEther("101.0"), "Owner should have max withdraw of 101 after depositing 1 TLOS and 100 TLOS being added as yield");
 
       await stlos.connect(addr1).depositTLOS({value: ethers.utils.parseEther("1.0")});
-      console.log("Checking max withdraw of addr1 to == 1");
+
+      const addr1Balance = await stlos.balanceOf(addr1.address);
+
+      expect(await stlos.previewRedeem(addr1Balance)).to.equal(ethers.utils.parseEther("1.0"), "Address 1 should be able to withdraw 1 TLOS after depositing 1 TLOS");
       expect(await stlos.maxWithdraw(addr1.address)).to.equal(ethers.utils.parseEther("1.0"), "Address 1 should be able to withdraw 1 TLOS after depositing 1 TLOS");
 
       expect(await stlos.maxWithdraw(owner.address)).to.equal(ethers.utils.parseEther("101.0"), "Owner should still have max withdrawl of 101 after address 1 deposited 1");
